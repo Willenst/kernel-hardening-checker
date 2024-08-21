c87393a0ece4e2380e066bf882ab0a034fadc642
Willenst <willenst@gmail.com>
2024-08-21 16:40:18 +0200

    test

--- kernel_hardening_checker/test_engine.py
+++ kernel_hardening_checker/test_engine.py
@@ -17,8 +17,8 @@ import sys
 import json
 import inspect
 from typing import Union, Optional, List, Dict, Tuple
-from kernel_hardening_checker.engine import StrOrBool, ChecklistObjType, KconfigCheck, CmdlineCheck, SysctlCheck, VersionCheck, OR, AND
-from kernel_hardening_checker.engine import populate_with_data, perform_checks, override_expected_value, colorize_result, print_unknown_options
+from .engine import StrOrBool, ChecklistObjType, KconfigCheck, CmdlineCheck, SysctlCheck, VersionCheck, OR, AND
+from .engine import populate_with_data, perform_checks, override_expected_value, colorize_result, print_unknown_options
 
 
 ResultType = List[Union[Dict[str, StrOrBool], str]]
