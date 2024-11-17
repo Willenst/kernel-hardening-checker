f626d33e91dada797895626bb77406add277df45
Willenst <willenst@gmail.com>
2024-11-17 14:19:55 +0200

    remove path variable and move to another folder

--- kernel_hardening_checker/config_files/distros/get_configs.sh
+++ kernel_hardening_checker/config_files/distros/get_configs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-path="kernel_hardening_checker/config_files/distros"
-
 # Links with raw content
 links=(
     "https://raw.githubusercontent.com/oracle/kconfigs/refs/heads/gh-pages/out/Android 14 (6.1) aarch64/config"
@@ -25,16 +23,16 @@ for url in "${links[@]}"; do
     distro_name="${folder_link##*/}" # Extract distro name
     cleaning_stage_2=$(echo ${distro_name// /_}) # Replace spaces with underscores
     filename="${cleaning_stage_2}.config" # Create file name
-    target="${path}/${filename}"
+    target="${filename}"
     wget -O "$target" "$url" # Fetch kconfig
     mv "config" "${target}" #
 done
 
 # Fetch some other kconfigs
-wget -O ${path}/Clearlinux_x86-64.config https://raw.githubusercontent.com/clearlinux-pkgs/linux/master/config
-wget -O ${path}/Clearlinux_x86-64.cmdline https://raw.githubusercontent.com/clearlinux-pkgs/linux/master/cmdline
-wget -O ${path}/OpenSUSE_x86-64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/master/config/x86_64/default
-wget -O ${path}/OpenSUSE_aarch64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/master/config/arm64/default
-wget -O ${path}/SLE-15-SP7_x86-64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/SLE15-SP7/config/x86_64/default
-wget -O ${path}/SLE-15-SP7_aarch64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/SLE15-SP7/config/x86_64/default
-wget -O ${path}/Azure_linux_x86_64.config https://raw.githubusercontent.com/microsoft/azurelinux/refs/heads/1.0/SPECS/kernel/config
+wget -O Clearlinux_x86-64.config https://raw.githubusercontent.com/clearlinux-pkgs/linux/master/config
+wget -O Clearlinux_x86-64.cmdline https://raw.githubusercontent.com/clearlinux-pkgs/linux/master/cmdline
+wget -O OpenSUSE_x86-64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/master/config/x86_64/default
+wget -O OpenSUSE_aarch64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/master/config/arm64/default
+wget -O SLE-15-SP7_x86-64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/SLE15-SP7/config/x86_64/default
+wget -O SLE-15-SP7_aarch64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/SLE15-SP7/config/x86_64/default
+wget -O Azure_linux_x86_64.config https://raw.githubusercontent.com/microsoft/azurelinux/refs/heads/1.0/SPECS/kernel/config
